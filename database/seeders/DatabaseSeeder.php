9a44720634344b5ab635747b175001eb2668834b
Tuyisenge2 <tuyisengetito3@gmail.com>
2025-04-22 14:44:37 +0200

    ft-products fecting

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        \App\Models\Products::factory(10)->create();
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('products', [ProductsController::class, 'index'])->name('products.index');
+Route::get('products/{product}', [ProductsController::class, 'show'])->name('products.show');
+Route::post('products', [ProductsController::class, 'store'])->name('products.store');
+Route::put('products/{product}', [ProductsController::class, 'update'])->name('products.update');
+Route::delete('products/{product}', [ProductsController::class, 'destroy'])->name('products.destroy');
